a67258e1940cf156712cd1f05a2fda466d48acfe
484Irgang <484gui@gmail.com>
2022-11-18 21:47:48 -0300

    Criando formulário

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import './style.css'
 
 function Form(){
+const [inputNum, setInputNum] = useState();
+function preencherCampoNum(e){
+    const countNum = e.target.value.split('');
+    if(countNum.length >= 4  && countNum.length <= 8){
+        console.log('ok')
+    }
+    console.log(countNum);
+}
     return(
         <div className='form'>
-            <p>cardholder name</p>
-            <input type="text" id="name"/>
-            <p>card number</p>
-            <input type="text" id="number"/>
-            <div className='date-exp'>
-                <p>exp. date (mm/yy)</p>
-                <input type="text" className='date-month'/>
-                <input type="text" className='date-year'/>
-            </div>
-            <span>cvc</span>
-            <input type="text" id="cvc"/>
-            <button>Confirm</button>
+            <form>
+                <p>cardholder name</p>
+                <input type="text" id="name" placeholder='e.g. Jane Aplessed'/>
+                <p>card number</p>
+                <input type="text" id="number" value={inputNum} onChange={(e) => preencherCampoNum(e)} placeholder='e.g. 1234 5678 9123 0000'/>
+                <div className='box-date-cvc'>
+                    <div className='date-exp'>
+                        <p>exp. date (mm/yy)</p>
+                        <input type="text" className='date-month' placeholder='MM'/>
+                        <input type="text" className='date-year' placeholder='YY'/>
+                    </div>
+                    <div className='box-cvc'>
+                        <p>cvc</p>
+                        <input type="text" id="cvc" placeholder='e.g. 123'/>
+                    </div>
+                </div>
+                <button>Confirm</button>
+            </form>    
         </div>
     )
 }
